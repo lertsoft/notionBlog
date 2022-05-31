27ad5dd407ca76c6b68850334ff15df27edaee77
Ronny Coste <ronny21.97@gmail.com>
2022-05-30 20:50:30 -0400

    first commit

---
+++ components/Footer.tsx
@@ -0,0 +1,120 @@
+import * as React from 'react'
+import { FaTwitter } from '@react-icons/all-files/fa/FaTwitter'
+import { FaInstagram } from '@react-icons/all-files/fa/FaInstagram'
+import { FaGithub } from '@react-icons/all-files/fa/FaGithub'
+import { FaLinkedin } from '@react-icons/all-files/fa/FaLinkedin'
+import { IoSunnyOutline } from '@react-icons/all-files/io5/IoSunnyOutline'
+import { IoMoonSharp } from '@react-icons/all-files/io5/IoMoonSharp'
+
+import { useDarkMode } from 'lib/use-dark-mode'
+import * as config from 'lib/config'
+
+import styles from './styles.module.css'
+
+// TODO: merge the data and icons from PageSocial with the social links in Footer
+
+export const FooterImpl: React.FC = () => {
+  const [hasMounted, setHasMounted] = React.useState(false)
+  const { isDarkMode, toggleDarkMode } = useDarkMode()
+
+  const onToggleDarkMode = React.useCallback(
+    (e) => {
+      e.preventDefault()
+      toggleDarkMode()
+    },
+    [toggleDarkMode]
+  )
+
+  React.useEffect(() => {
+    setHasMounted(true)
+  }, [])
+
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.copyright}>Copyright 2022 {config.author}</div>
+
+      <div className={styles.settings}>
+        {hasMounted && (
+          <a
+            className={styles.toggleDarkMode}
+            href='#'
+            role='button'
+            onClick={onToggleDarkMode}
+            title='Toggle dark mode'
+          >
+            {isDarkMode ? <IoMoonSharp /> : <IoSunnyOutline />}
+          </a>
+        )}
+      </div>
+
+      <div className={styles.social}>
+        {config.twitter && (
+          <a
+            className={styles.twitter}
+            href={`https://twitter.com/${config.twitter}`}
+            title={`Twitter @${config.twitter}`}
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <FaTwitter />
+          </a>
+        )}
+
+        {config.instagram && (
+          <a
+            className={styles.instagram}
+            href={`https://instagram.com/${config.instagram}`}
+            title={`Instagram @${config.instagram}`}
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <FaInstagram />
+          </a>
+        )}
+
+        {config.github && (
+          <a
+            className={styles.github}
+            href={`https://github.com/${config.github}`}
+            title={`GitHub @${config.github}`}
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <FaGithub />
+          </a>
+        )}
+
+        {config.linkedin && (
+          <a
+            className={styles.linkedin}
+            href={`https://www.linkedin.com/in/${config.linkedin}`}
+            title={`LinkedIn ${config.author}`}
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <FaLinkedin />
+          </a>
+        )}
+
+        {config.linkedin && (
+          <a
+            className={styles.personal}
+            href={`https://www.ronnycoste.com`}
+            title={`${config.author} Personal Site`}
+            target='_blank'
+            rel='noopener noreferrer'
+            
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="32" fill="currentColor" viewBox="0 0 16 16">
+              <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+              <path d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+            </svg>
+            
+          </a>
+        )}
+      </div>
+    </footer>
+  )
+}
+
+export const Footer = React.memo(FooterImpl)
---
+++ site.config.ts
@@ -0,0 +1,62 @@
+import { siteConfig } from './lib/site-config'
+
+export default siteConfig({
+  // the site's root Notion page (required)
+  // https://costeronny.notion.site/Ronny-s-Mind-3914c9b0241c4d3e85e2ff448e014230
+  rootNotionPageId: '3914c9b0241c4d3e85e2ff448e014230',
+
+  // if you want to restrict pages to a single notion workspace (optional)
+  // (this should be a Notion ID; see the docs for how to extract this)
+  rootNotionSpaceId: null,
+
+  // basic site info (required)
+  name: 'Ronny mind',
+  domain: 'https://blog.ronnycoste.com',
+  author: 'Ronny Coste',
+  // open graph metadata (optional)
+  description: 'Personal Blog of Ronny ',
+
+  // social usernames (optional)
+  twitter: 'costeronny',
+  github: 'lertsoft',
+  linkedin: 'ronnycoste',
+  instagram: 'costeronny',
+
+  // default notion icon and cover images for site-wide consistency (optional)
+  // page-specific values will override these site-wide defaults
+  defaultPageIcon: 'https://blog.ronnycoste.com/page-icon.png',
+  defaultPageCover: 'https://blog.ronnycoste.com/page-cover.jpg',
+  defaultPageCoverPosition: 0.5,
+
+  // whether or not to enable support for LQIP preview images (optional)
+  isPreviewImageSupportEnabled: true,
+
+  // whether or not redis is enabled for caching generated preview images (optional)
+  // NOTE: if you enable redis, you need to set the `REDIS_HOST` and `REDIS_PASSWORD`
+  // environment variables. see the readme for more info
+  isRedisEnabled: false,
+
+  // map of notion page IDs to URL paths (optional)
+  // any pages defined here will override their default URL paths
+  // example:
+  //
+  // pageUrlOverrides: {
+  //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
+  //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
+  // }
+  pageUrlOverrides: null,
+
+  // pageUrlAdditions: {
+  //   '/the-social-audio-revolution': 'c4deaf33cc924ad7a5b9f69c6ae04a01'
+  // },
+
+  // whether to use the default notion navigation style or a custom one with links to
+  // important pages
+  navigationStyle: 'custom',
+  navigationLinks: [
+    {
+      title: 'Bucketlist',
+      pageId: '62c28041a66f4b9798c3a523a2368775'
+    },
+  ]
+})
